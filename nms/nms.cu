32a16693f1472b1d92985f4b6b015d437e6cb382
dssa <lawphi56@gmail.com>
2018-02-07 21:45:39 +0000

    added cpu version

--- nms/nms.cu
+++ nms/nms.cu
@@ -10,66 +10,66 @@ __global__ void nms_kern(float* boxes, int* elims, int* mask,
                        float thresh, int n_bxs)
 {
 
-int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  int idx = threadIdx.x + blockDim.x * blockIdx.x;
 
-if(idx >= (n_bxs *  (n_bxs - 1))/2)
-{
-  return;
-}
-
-int box_x_idx = n_bxs - static_cast<int>(sqrt((n-1/2)*(n-1/2)-2*idx));
-int box_y_idx = idx + 1 + (box_x_idx * (box_x_idx+1))/2 - box_x_idx * (n_bxs-1);
-
-float box_x[4], box_y[4];
-
-for(int i=0; i<4; i++)
-{
-  box_x[i] = boxes[box_x_idx*4 + i];
-  box_y[i] = boxes[box_y_idx*4 + i];
-}
-
-float rightmost_l = fmax(box_x[0], box_y[0]);
-float leftmost_r = fmin(box_x[0] + box_x[2], box_y[0] + box_y[2]);
-float delta_x = fmax(0., leftmost_r - rightmost_l);
-
-float bottommost_tp = fmax(box_x[1], box_y[1]);
-float topmost_b = fmin(box_x[1] + box_x[3], box_y[1] + box_y[3]);
-float delta_y = fmax(0., topmost_b - bottommost_tp);
-
-float uni = box_x[2] * box_x[3] + box_y[2] * box_y[3];
-
-float iou = delta_x * delta_y / (uni - delta_x * delta_y);
-
-if(iou < thresh)
-{
-  elims[idx] = 1;
-}
-
-__syncthreads();
-
-if(box_x_idx != 0)
-{
-  return;
-}
-
-int col = 0, row = 0;
-while(col < box_y_idx)
-{
-  mask[box_y_idx] *= elims[elim_idx(col, box_y_idx, n_bxs)];
-  while(row + col < n_bxs - 1)
+  if(idx >= (n_bxs *  (n_bxs - 1))/2)
   {
-    if(mask[row + col + 1] == 0)
-    {
-      row += 1;
-    }
-    else
-    {
-      break;
-    }
+    return;
   }
-  col += row + 1;
-  row = 0;
-}
+
+  int box_x_idx = n_bxs - static_cast<int>(sqrt((n-1/2)*(n-1/2)-2*idx));
+  int box_y_idx = idx + 1 + (box_x_idx * (box_x_idx+1))/2 - box_x_idx * (n_bxs-1);
+
+  float box_x[4], box_y[4];
+
+  for(int i=0; i<4; i++)
+  {
+    box_x[i] = boxes[box_x_idx*4 + i];
+    box_y[i] = boxes[box_y_idx*4 + i];
+  }
+
+  float rightmost_l = fmax(box_x[0], box_y[0]);
+  float leftmost_r = fmin(box_x[0] + box_x[2], box_y[0] + box_y[2]);
+  float delta_x = fmax(0., leftmost_r - rightmost_l);
+
+  float bottommost_tp = fmax(box_x[1], box_y[1]);
+  float topmost_b = fmin(box_x[1] + box_x[3], box_y[1] + box_y[3]);
+  float delta_y = fmax(0., topmost_b - bottommost_tp);
+
+  float uni = box_x[2] * box_x[3] + box_y[2] * box_y[3];
+
+  float iou = delta_x * delta_y / (uni - delta_x * delta_y);
+
+  if(iou < thresh)
+  {
+    elims[idx] = 1;
+  }
+
+  __syncthreads();
+
+  if(box_x_idx != 0)
+  {
+    return;
+  }
+
+  int col = 0, row = 0;
+  while(col < box_y_idx)
+  {
+    mask[box_y_idx] *= elims[elim_idx(col, box_y_idx, n_bxs)];
+    while(row + col < n_bxs - 1)
+    {
+      if(mask[row + col + 1] == 0)
+      {
+        row += 1;
+      }
+      else
+      {
+        break;
+      }
+    }
+    col += row + 1;
+    row = 0;
+  }
 
 }
 
