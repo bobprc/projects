19c67d55f28f24a31f45650d5a07d6efd9a65368
dssa <lawphi56@gmail.com>
2018-02-20 21:28:01 +0000

    deleted some junk

--- nms/nms.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-
-std::vector<int> nms(const std::vector<std::vector<float> > & boxes,
-                     const std::vector<float> & scores,
-                     const float thresh);
--- nms/non_max_s.pyx
+++
@@ -1,7 +0,0 @@
-from libcpp.vector cimport vector
-
-cdef extern from "nms.h":
-    vector[int] nms(vector[vector[float]], vector[float], float)
-
-def non_max_s(boxes, scores, thresh):
-    return nms(boxes, scores, thresh)
